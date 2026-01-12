d0c4d137efc340d66278048b8151d929b63e7c5c
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2026-01-12 11:56:32 -0500

    Run clang-format. Add VM stack over/underflow checking. Disable optimizations because inlining makes it harder to debug

--- interpreter/interpreter.c
+++ interpreter/interpreter.c
@@ -1,17 +1,43 @@
 #define _GNU_SOURCE
 
-#include <stdlib.h> // for EXIT_*, exit
-#include <stdio.h> // for getdelim, stdin, EOF, printf
-#include <sys/mman.h> // for mmap, PROT_*, MAP_*
-#include <stdint.h> // for uint64_t
 #include <inttypes.h> // for PRIu64
-#include <signal.h> // for sigset_t, sigfillset, sigprocmask, SIG_SETMASK
+#include <signal.h>   // for sigset_t, sigfillset, sigprocmask, SIG_SETMASK
+#include <stddef.h>   // for NULL
+#include <stdint.h>   // for uint64_t
+#include <stdio.h>    // for getdelim, stdin, EOF, printf, stderr, fputs
+#include <stdlib.h>   // for EXIT_*, exit
+#include <sys/mman.h> // for mmap, PROT_*, MAP_*
 
 #include "constants.h"
 
-void interpret(void *ip, void *sp);
+[[noreturn]] void interpret(void *ip, void *sp);
 
-size_t const STACK_SIZE = 0x10000;
+bool is_valid_opcode(uint64_t const opcode) {
+    static uint64_t const OPCODES[] = {
+        0xadd1000, 0x50b1000, 0xd0d0000, 0x10ad000, 0x0add000, 0x050b000,
+        0x0a55000, 0x1001000, 0xe3e3000, 0xeeee000, 0x1234000, 0xb001000,
+        0x4321000, 0x7777000, 0xcaca000, 0xc701000, 0x170c000};
+    for (size_t i = 0; i < _Countof(OPCODES); i++) {
+        if (opcode == OPCODES[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool is_valid_bytecode(uint64_t const *const bytecode,
+                       ssize_t const bytecode_size) {
+    if (bytecode_size < 0 || bytecode_size % 16) {
+        return false;
+    }
+    ssize_t num_bytecode_words = bytecode_size / 16;
+    for (ssize_t i = 0; i < num_bytecode_words; i++) {
+        if (!is_valid_opcode(bytecode[i * 2])) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main(void) {
     // Block all signals because they clobber the red zone
@@ -19,17 +45,17 @@ int main(void) {
     sigfillset(&mask);
     sigprocmask(SIG_SETMASK, &mask, NULL);
 
-    char * bytecode = nullptr;
-    size_t bytecode_size;
-    ssize_t bytes_read = getdelim(&bytecode, &bytecode_size, EOF, stdin);
-    if (bytes_read < 0) {
-        return EXIT_FAILURE;
-    }
-    if (bytes_read % 8) {
+    uint64_t *bytecode = NULL;
+    size_t bytecode_allocation_size;
+    ssize_t bytes_read =
+        getdelim((char **)&bytecode, &bytecode_allocation_size, EOF, stdin);
+    if (!is_valid_bytecode(bytecode, bytes_read)) {
+        fputs("Invalid bytecode.\n", stderr);
         return EXIT_FAILURE;
     }
 
-    void * const stack = mmap(NULL, STACK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    void *const stack = mmap(NULL, STACK_SIZE_IN_BYTES, PROT_READ | PROT_WRITE,
+                             MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     if (!stack) {
         return EXIT_FAILURE;
     }
